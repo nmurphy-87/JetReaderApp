d06f2630063e513d4520c56ade2901828b05253e
Niall <niall@Aarons-MacBook-Air.local>
2024-01-26 17:53:04 +0000

    set up navigation screen enums

--- app/src/main/java/com/niallmurph/jetreaderapp/navigation/ReaderScreens.kt
+++ app/src/main/java/com/niallmurph/jetreaderapp/navigation/ReaderScreens.kt
@@ -1,5 +1,29 @@
 package com.niallmurph.jetreaderapp.navigation
 
-data class ReaderScreens(
-    val HomeScreen : String
-)
+import com.niallmurph.jetreaderapp.screens.SplashScreen
+
+enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    HomeScreen,
+    DetailsScreen,
+    StatsScreen,
+    SearchScreen,
+    UpdateScreen;
+    companion object{
+        fun fromRoute(route : String?) : ReaderScreens
+        = when (route?.substringBefore("/")){
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            HomeScreen.name -> HomeScreen
+            DetailsScreen.name -> DetailsScreen
+            StatsScreen.name -> StatsScreen
+            SearchScreen.name -> SearchScreen
+            UpdateScreen.name -> UpdateScreen
+            null -> HomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognised")
+        }
+    }
+}
--- app/src/main/java/com/niallmurph/jetreaderapp/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/niallmurph/jetreaderapp/screens/ReaderSplashScreen.kt
@@ -1,1 +1,8 @@
 package com.niallmurph.jetreaderapp.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen(){
+
+}
