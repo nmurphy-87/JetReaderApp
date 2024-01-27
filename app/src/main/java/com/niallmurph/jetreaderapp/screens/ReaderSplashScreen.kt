b1252a432a313b03b8f1bd933ae27e6849de4eae
Niall <niall@Aarons-MacBook-Air.local>
2024-01-27 11:58:58 +0000

    adding animation to the Splash Screen

--- app/src/main/java/com/niallmurph/jetreaderapp/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/niallmurph/jetreaderapp/screens/ReaderSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.niallmurph.jetreaderapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -7,22 +10,43 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun SplashScreen(navController: NavController= NavController(context = LocalContext.current)) {
+
+    val scale = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true){
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                }
+            )
+        )
+        delay(1500L)
+    }
+
     Surface(
         modifier = Modifier
             .padding(16.dp)
-            .size(320.dp),
+            .size(320.dp)
+            .scale(scale = scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(
