0b52156cd2ee8f3756bc527933587eb90c919ffe
Niall <niall@Aarons-MacBook-Air.local>
2024-02-01 17:19:27 +0000

    Set up Search screen top app bar

--- app/src/main/java/com/niallmurph/jetreaderapp/screens/search/ReaderSearchScreen.kt
+++ app/src/main/java/com/niallmurph/jetreaderapp/screens/search/ReaderSearchScreen.kt
@@ -1,8 +1,32 @@
 package com.niallmurph.jetreaderapp.screens.search
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import com.niallmurph.jetreaderapp.components.ReaderAppBar
 
+@Preview
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun SearchScreen(){
+fun SearchScreen(navController: NavController = NavController(LocalContext.current)) {
 
+    Scaffold(
+        topBar = {
+            ReaderAppBar(
+                title = "Search Books",
+                icon = Icons.Default.ArrowBack,
+                showProfile = false,
+                navController = navController
+            ){
+                navController.popBackStack()
+            }
+        }
+    ){
+
+    }
 }
